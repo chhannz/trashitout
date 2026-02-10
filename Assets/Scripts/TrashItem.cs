acf3f409b66b53455fe0a67929b6d2a62c3c64f9
chhannz <chhannz@gmail.com>
2026-02-10 10:38:54 +0700

    spawnScriptProses

---
+++ Assets/Scripts/TrashItem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Pool;
+public class TrashItem : MonoBehaviour
+{
+    private IObjectPool<TrashItem> _pool;
+
+    public void SetPool(IObjectPool<TrashItem> pool)
+    {
+        _pool = pool;
+    }
+
+    public void ReturnToPool()
+    {
+        if (_pool != null)
+        {
+            _pool.Release(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class TrashSpawner : MonoBehaviour
 {
+    [Header("Settings")] 
+    [SerializeField] private TrashItem trashPrefab;
+
+    [SerializeField] private float spawnInterval = 2f;
+
+    [SerializeField] private Vector2 spawnAreaMin; 
+    [SerializeField] private Vector2 spawnAreaMax;
+
+    private IObjectPool<TrashItem> trashPool;
+    private float nextSpawnTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void SpawnTrash()
     {
         
     }
