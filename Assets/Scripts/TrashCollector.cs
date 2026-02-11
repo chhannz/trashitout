af28a80051442289d4f51e8ff07897ea02de598f
chhannz <chhannz@gmail.com>
2026-02-11 10:47:14 +0700

    spawnMod1

--- Assets/Scripts/TrashCollector.cs
+++ Assets/Scripts/TrashCollector.cs
@@ -4,12 +4,12 @@ using UnityEngine.Events;
 
 public class TrashCollector : MonoBehaviour
 {
-    public UnityEvent OnTrashCollected;
+    //public UnityEvent OnTrashCollected;
     private void OnTriggerEnter2D(Collider2D obj)
     {
         if(obj.CompareTag("trashbag"))
         {
-            OnTrashCollected?.Invoke();
+            //OnTrashCollected?.Invoke();
             
             TrashItem item = obj.GetComponent<TrashItem>();
             if (item != null)
